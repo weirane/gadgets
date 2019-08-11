64043fdebdc26484acd1ef20c357d5ac10bffc74
Wang Ruochen <wangkevin920@163.com>
2019-08-11 18:26:54 +0800

    Use a file to hold graph data

--- c-cpp/bellman-ford/README.md
+++ c-cpp/bellman-ford/README.md
@@ -6,3 +6,22 @@ This project uses [catch](https://github.com/catchorg/Catch2) as the unit test t
     sudo pacman -S catch2
 
 to install it, or you can download the [single header version](https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp) to `catch2/catch.hpp` and then add `-I.` to `CXXFLAGS` in the `Makefile`.
+
+To run the algorithm (not needed for testing), the graph data should be put in the file `./data`, or specified in `argv[1]`, which should have the following structure:
+
+    <graph node count> <source node for B-F>
+    <edge-from> <edge-to> <edge-weight>
+    <edge-from> <edge-to> <edge-weight>
+    ...
+
+Example:
+
+    5 0
+    0 1 -1
+    0 2 4
+    1 2 3
+    1 3 2
+    1 4 2
+    3 2 5
+    3 1 1
+    4 3 -3
--- c-cpp/bellman-ford/main.cpp
+++ c-cpp/bellman-ford/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "graph.h"
 
 using namespace std;
@@ -6,18 +7,33 @@ using namespace std;
 auto bellman_ford(const Graph &graph, int start, vector<double> &dist,
                   vector<int> &prev, bool &neg_cycle) -> bool;
 
-int main() {
-    auto g = Graph::with_node_count(4);
-    g.add_edge(0, 1, 1);
-    g.add_edge(0, 2, 3);
-    g.add_edge(1, 2, 1);
-    g.add_edge(2, 0, 2);
+int main(int argc, char *argv[]) {
+    ifstream data;
+    if (argc > 1)
+        data = ifstream(argv[1]);
+    else
+        data = ifstream("data");
+    if (!data.good()) {
+        cout << "Error open file `data'" << endl;
+        return 1;
+    }
+
+    int node_cnt, start;
+    int from, to;
+    double weight;
+
+    data >> node_cnt >> start;
+    auto g = Graph::with_node_count(node_cnt);
+
+    while (data >> from >> to >> weight) {
+        g.add_edge(from, to, weight);
+    }
 
     auto dist = vector<double>{};
     auto prev = vector<int>{};
     bool neg_cycle;
 
-    if (!bellman_ford(g, 0, dist, prev, neg_cycle)) {
+    if (!bellman_ford(g, start, dist, prev, neg_cycle)) {
         cout << "Error occurs" << endl;
         return 1;
     }
