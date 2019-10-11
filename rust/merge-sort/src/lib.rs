dc771428c5e86a3660f5fc4c81b9926a3196c004
Wang Ruochen <wangkevin920@163.com>
2019-10-11 19:56:35 +0800

    Make merge-sort faster by avoiding copying

--- rust/merge-sort/src/lib.rs
+++ rust/merge-sort/src/lib.rs
@@ -2,64 +2,84 @@
 
 #![feature(test)]
 
-fn merge<T>(array: &mut [T], start: usize, mid: usize, end: usize, space: &mut Vec<T>)
+use std::ptr;
+
+fn merge<T>(array: &mut [T], start: usize, mid: usize, end: usize)
 where
-    T: Copy + PartialOrd,
+    T: PartialOrd,
 {
     let mut left = start;
     let mut right = mid;
-    let mut i = start;
+    let mut i = 0;
+    let mut space = Vec::<T>::with_capacity(end - start);
+
+    unsafe {
+        space.set_len(end - start);
+    }
+
     while left < mid && right < end {
         if array[left] <= array[right] {
-            space[i] = array[left];
+            unsafe {
+                // space[i] = array[left];
+                ptr::copy_nonoverlapping(array.as_ptr().add(left), space.as_mut_ptr().add(i), 1);
+            }
             left += 1;
         } else {
-            space[i] = array[right];
+            unsafe {
+                // space[i] = array[right];
+                ptr::copy_nonoverlapping(array.as_ptr().add(right), space.as_mut_ptr().add(i), 1);
+            }
             right += 1;
         }
         i += 1;
     }
-    if left < mid {
-        (left..mid).for_each(|j| {
-            space[i] = array[j];
-            i += 1;
-        });
-    } else {
-        (right..end).for_each(|j| {
-            space[i] = array[j];
-            i += 1;
-        });
-    }
 
-    (start..end).for_each(|i| array[i] = space[i]);
+    unsafe {
+        if left < mid {
+            // space[i..] = array[left..mid]
+            ptr::copy_nonoverlapping(
+                array.as_ptr().add(left),
+                space.as_mut_ptr().add(i),
+                mid - left,
+            );
+        } else {
+            // space[i..] = array[right..end]
+            ptr::copy_nonoverlapping(
+                array.as_ptr().add(right),
+                space.as_mut_ptr().add(i),
+                end - right,
+            );
+        }
+
+        // array[start..end] = space[..]
+        ptr::copy_nonoverlapping(space.as_ptr(), array.as_mut_ptr().add(start), end - start);
+    }
 }
 
 /// Inner function used by `merge_sort`. Note that `array[end]` is not included
 /// in the elements to be sorted.
-fn merge_inner<T>(array: &mut [T], start: usize, end: usize, space: &mut Vec<T>)
+fn merge_inner<T>(array: &mut [T], start: usize, end: usize)
 where
-    T: Copy + PartialOrd,
+    T: PartialOrd,
 {
     if start + 1 >= end {
         return;
     }
     let mid = start + (end - start) / 2;
-    merge_inner(array, start, mid, space);
-    merge_inner(array, mid, end, space);
-    merge(array, start, mid, end, space);
+    merge_inner(array, start, mid);
+    merge_inner(array, mid, end);
+    merge(array, start, mid, end);
 }
 
 /// Merge sort algorithm.
 pub fn merge_sort<T>(array: &mut [T])
 where
-    T: Copy + PartialOrd,
+    T: PartialOrd,
 {
-    let len = array.len();
-    if len < 2 {
-        return;
+    match array.len() {
+        0 | 1 => (),
+        len => merge_inner(array, 0, len),
     }
-    let mut space = vec![array[0]; len];
-    merge_inner(array, 0, len, &mut space);
 }
 
 #[cfg(test)]
