29ae398916242b57ef1f4f67a40c22f1d5467dde
Wang Ruochen <wangkevin920@163.com>
2020-06-18 11:21:45 +0800

    Add more notes

--- rust/sound/README.md
+++ rust/sound/README.md
@@ -5,4 +5,8 @@ Make some sound with Rust. Play it with
     cargo run
     ffplay -nodisp -f f32le -ar 48000 output.bin
 
+Convert to mp3:
+
+    ffmpeg -f f32le -ar 48000 -i output.bin out.mp3
+
 [vid]: https://www.youtube.com/watch?v=FYTZkE5BZ-0
--- rust/sound/src/main.rs
+++ rust/sound/src/main.rs
@@ -9,16 +9,17 @@ static mut BPM: f32 = 80.;
 
 fn wave(note: f32, beats: f32) -> Vec<u8> {
     let omega = note * 2. * PI / SAMPLE_RATE;
+    // not using multi-threading so the unsafe here is safe
     let one_beat = 60. / unsafe { BPM } * SAMPLE_RATE;
-    let count = beats * one_beat;
+    let duration = beats * one_beat;
     // duration for attack and release
-    let att_rel = count.min(one_beat) / 5.;
+    let att_rel = duration.min(one_beat) / 5.;
 
     let mut notes = Vec::new();
-    for n in 0..count as i32 {
+    for n in 0..duration as i32 {
         let n = n as f32;
         // sine wave with attack and release
-        let y = (n * omega).sin() * 1f32.min(n / att_rel) * 1f32.min((count - n) / att_rel);
+        let y = (n * omega).sin() * 1f32.min(n / att_rel) * 1f32.min((duration - n) / att_rel);
         notes.extend(y.to_le_bytes().iter().cloned());
     }
     notes
@@ -50,7 +51,27 @@ fn main() -> io::Result<()> {
         B4, 1./3.; D5, 1./3.; F5s, 1./3.; G4s, 1.5; D4s, 0.25; E4, 0.25; F4s, 0.25;
         G4s, 0.25; A4, 0.25; B4, 0.25; E5, 0.25;
 
-        E5, 1.5;
+        E5, 1.25; F5, 0.25; F5s, 0.25; G5s, 0.25; A5, 0.5; B5, 0.375; B5, 0.125; C5s, 1.5;
+
+        E5, 0.25; REST, 0.125; E5, 0.125; G4, 1.5; B4, 0.25; REST, 0.125; B4, 0.125;
+        C4s, 0.5; G4, 0.25; REST, 0.125; G4, 0.125;
+
+        B3, 0.5; E4, 0.25; REST, 0.125; E4, 0.125; B3f, 0.5; C4s, 0.25; REST, 0.125; C4s, 0.125;
+        A3, 0.5; B3f, 0.25; REST, 0.125; B3f, 0.125; G3s, 0.5; B3f, 0.25; REST, 0.125; B3f, 0.125;
+
+        A3, 0.5; B3f, 0.25; REST, 0.125; B3f, 0.125; G3s, 0.5; B3f, 0.25; REST, 0.125; B3f, 0.125;
+        A3, 0.5; B3f, 0.25; REST, 0.125; B3f, 0.125; G3s, 0.5; A3, 0.25; REST, 0.125; B3f, 0.125;
+
+        G3s, 0.5; A3, 0.25; REST, 0.125; B3f, 0.125; G3s, 0.5; A3, 0.25; REST, 0.125; B3f, 0.125;
+        G3s, 1./6.; A3, 1./6.; B3f, 1./6.; G3s, 1./6.; A3, 1./6.; B3f, 1./6.;
+        G3s, 1./6.; A3, 1./6.; B3f, 1./6.; G3s, 1./6.; A3, 1./6.; B3f, 1./6.;
+
+        G3s, 1./6.; A3, 1./6.; B3f, 1./6.; B3, 1./6.; C4, 1./6.; C4s, 1./6.;
+        D4, 1./6.; D4s, 1./6.; E4, 1./6.; F4, 1./6.; F4s, 1./6.; G4, 1./6.;
+        G4s, 1./6.; A4, 1./6.; B4f, 1./6.; B4, 1./6.; C5, 1./6.; C5s, 1./6.;
+        D5, 0.25; D5s, 0.25; E5, 0.25; F5, 0.25;
+
+        F5s, 0.25; REST, 0.25; F5s, 2.;
     }
     Ok(())
 }
